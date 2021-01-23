de503cea6e4697a63c8fd4c3290b305248c38491
Kostya <47311709+stressout@users.noreply.github.com>
2021-01-23 13:52:30 +0300

    Update ZipCommand.java

--- Archiver/src/command/ZipCommand.java
+++ Archiver/src/command/ZipCommand.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Enter full file name a archive:");
+        ConsoleHelper.writeMessage("Enter full file name of archive:");
         Path zipPath = Paths.get(ConsoleHelper.readString());
         return new ZipFileManager(zipPath);
     }
